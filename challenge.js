bea49018708516b295c3b3e923ec6a529012c6d1
djones1117 <socjones21@yahoo.com>
2023-11-14 15:00:51 -0500

    setup daily coding challenges

---
+++ challenge.js
@@ -0,0 +1,31 @@
+// Challenge: 00-sayHello (example)
+// Difficulty: Basic
+// Prompt:
+// Write a function called sayHello that returns the string "Hello!".
+// Examples:
+// sayHello() //=> Hello!
+/*-----------------------------------------------------------------*/
+// Your solution for 00-sayHello (example) here
+
+function sayHello() {
+  return "Hello!";
+}
+
+console.log(sayHello());
+
+
+
+// Challenge: greet (example)
+//Difficulty: Basic
+//Prompt:
+//Write a function called greet that takes a name as an argument and returns the string "Hello, John!"
+//Examples:
+//greet("Dylan") => Hello Dylan!
+/*-----------------------------------------------------------------*/
+// Your solution for the greet example here
+
+function greet(name) {
+  console.log("hello , " + name + "!");
+}
+
+greet("Dylan");
---
+++ solution.js
@@ -0,0 +1,11 @@
+function sayHello() {
+  return "Hello!";
+}
+
+console.log(sayHello());
+
+function greet(name) {
+  console.log("hello , " + name + "!");
+}
+
+greet("Dylan");
